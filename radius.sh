efd8ffc090e37bf83da558708ca0bb062f21b9c7
shirin-shaaa <shaaa.shirin@gmail.com>
2026-02-04 14:27:09 +0530

    Add files via upload

---
+++ radius.sh
@@ -0,0 +1,8 @@
+echo " Enter the Radius of the circle:"
+read r
+pi=314
+area=$((pi*r*r / 100))
+circumference=$((2*pi*r/100))
+echo "Area of circle = $area"
+echo "Circumference of circle = $circumference"
+
